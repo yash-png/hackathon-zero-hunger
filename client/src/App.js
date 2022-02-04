e8450d8c1cf859accd0d04f2fa7b4e52cd85a659
yash-png <65919365+yash-png@users.noreply.github.com>
2022-02-04 08:25:26 +0530

    Init commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,31 +4,25 @@ import Home from "./components/Home";
 import Map from "./components/Map";
 import Register from "./components/Register";
 import Registeruser from "./components/Registeruser";
-import Donation from "./components/Donation";
 import Organization from "./components/Organization";
 import About from "./components/About";
 import Contact from "./components/Contact";
 
 function App() {
-  // const [data,setData] = useState("");
-
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home> </Home>} />
         <Route path="/map" element={<Map> </Map>} />
         <Route path="/register" element={<Register> </Register>} />
-        <Route path="/registeruser" element={<Registeruser> </Registeruser>} />
-        {/* <Route exact path="/donation" element={<Donation> </Donation>} />  */}
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/about" element={<About />} />
         <Route
-          exact
-          path="/organizations/:name"
-          element={<Organization />}
-        />{" "}
+          path="/registeruser"
+          element={<Registeruser> </Registeruser>}
+        />
+        <Route path="/contact" element={<Contact />} />{" "}
+        <Route path="/about" element={<About />} />{" "}
+        <Route exact path="/organizations/:name" element={<Organization />} />{" "}
       </Routes>{" "}
-     
     </Router>
   );
 }
